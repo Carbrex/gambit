338ac51d23d4f27c43246fb04ee742b87e9ac261
jebuAsus <95964955+Carbrex@users.noreply.github.com>
2022-10-28 23:06:32 +0530

    28.10.2022 23.06 made Navbar

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <main class="page">
+      {/* <!-- header --> */}
+      <header class="hero">
+        <div class="hero-container">
+          <div class="hero-text">
+            <h1>NiceSite</h1>
+            <h4>no fuss, only good</h4>
+          </div>
+        </div>
+      </header>
+      
+    </main>
+  )
+}
+
+export default Home
---
+++ src/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import image from "./Gambit.png";
+
+const Navbar = () => {
+    const [showLinks, setShowLinks] = React.useState(false);
+    return (
+        <nav className="navbar">
+            <div className="nav-center">
+                <div className="nav-header">
+                    <Link to="/">
+                        <div className="nav-logo">
+                            <img src={image} alt="gambit" />
+                        </div>
+                    </Link>
+                    <button class="btn nav-btn" onClick={()=>setShowLinks(!showLinks)}><i class="fas fa-align-justify"></i></button>
+                </div>
+                <div type="button" class={`nav-links ${showLinks?'show-links':null}`}>
+                    <Link to="/" class="nav-link">home</Link>
+                    <Link to="/play" class="nav-link">play</Link>
+                    <Link to="/about" class="nav-link">about</Link>
+                    {/* <Link to="/" class="nav-link">tags</Link> */}
+                    {/* <Link to="/" class="nav-link">Home</Link> */}
+                    <div class="nav-link contact-link">
+                        <Link to="/" class="btn">Contact</Link>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
