63e1ad62a54c09c5c8c1baff67e112d799201d1f
Carbrex <95964955+Carbrex@users.noreply.github.com>
2023-03-30 13:12:25 +0000

    Changed src link in About Page

--- frontend/src/Pages/About.js
+++ frontend/src/Pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <section className='about'>
       <img src={image} alt="gambit" />
       <p>
-        Welcome to Gambit, a chess website where you can play chess with your friends while chatting in real-time, or watch top games from lichess.org, a popular chess website. Our platform is built using React on the frontend and Node.js with Express and WebSockets on the backend, developed by <a href="https://github.com/Carbrex">Carbrex</a> (Lakshya Kumar). You can find the source code <a href="https://github.com/Carbrex">here</a>, where you are welcome to contribute to the project or report any issues you encounter.<br/>
+        Welcome to Gambit, a chess website where you can play chess with your friends while chatting in real-time, or watch top games from lichess.org, a popular chess website. Our platform is built using React on the frontend and Node.js with Express and WebSockets on the backend, developed by <a href="https://github.com/Carbrex">Carbrex</a> (Lakshya Kumar). You can find the source code <a href="https://github.com/Carbrex/gambit">here</a>, where you are welcome to contribute to the project or report any issues you encounter.<br/>
         We hope you enjoy playing chess on Gambit as much as we enjoyed creating it!
       </p>
     </section>
